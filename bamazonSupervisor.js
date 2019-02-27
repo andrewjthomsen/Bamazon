ced76dd9ec758397f7595f6c40494f2aa4140886
andrew <andrewjthomsen@yahoo.com>
2019-02-26 21:36:36 -0800

    added psuedo code to bamazonSupervisor.js

---
+++ bamazonSupervisor.js
@@ -0,0 +1,5 @@
+// - createConnection
+// - Function to display "Product Sales by Department" via JOIN
+// - Function to send an INSERT query to "Create New Product"
+// - Core inquirer call to show options (just like before)
+// - 
